b12758f696e6ca9e0e08ecf72945f42af26bf153
giyun <parkgy94@gmail.com>
2019-07-25 22:07:43 +0900

    feat added inputClass to get cars name and trycount

---
+++ IdeaProjects/racingCar/src/main/java/view/inputView.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.util.Scanner;
+
+public class inputView {
+
+    static final String COMMA = ",";
+
+    Scanner scanner = new Scanner(System.in);
+
+    public String[] inputNumOfCar() {
+        System.out.println("자동차 이름들을 입력하세요.( , 로 구분지어 입력하세요) ");
+        return scanner.nextLine().split(COMMA);
+    }
+
+    public int inputNumOfTry() {
+        System.out.println("시도할 횟수를 입력하세요.");
+        return scanner.nextInt();
+    }
+}
