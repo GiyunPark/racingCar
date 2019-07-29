8e8b96254c48d70e64daddf52c9cd3256abab636
giyun <parkgy94@gmail.com>
2019-07-29 22:56:41 +0900

    Feat added for loop for the numOfTry
    the for loop call singleGame and show currentresult method

--- IdeaProjects/racingCar/src/main/java/RacingCarMain.java
+++ IdeaProjects/racingCar/src/main/java/RacingCarMain.java
@@ -1,10 +1,26 @@
+import model.RacingCarFactory;
+import model.RacingCars;
+import model.RacingGameManager;
 import view.InputView;
+import view.ResultView;
 
 public class RacingCarMain {
 
     public static void main(String[] args){
         InputView inputView = new InputView();
-        String[] numOfCars = inputView.inputNumOfCar();
+        String[] nameOfCars = inputView.inputNumOfCar();
         int numOfTry = inputView.inputNumOfTry();
+
+        RacingCarFactory racingCarFactory = new RacingCarFactory();
+        RacingGameManager racingGameManager = new RacingGameManager();
+
+        RacingCars racingCars = racingCarFactory.CarFactory(nameOfCars);
+        ResultView resultView = new ResultView();
+
+        for(int tryCount=0; tryCount<numOfTry; tryCount++){
+            racingGameManager.singleGame(racingCars);
+            resultView.showCurrentResult(racingCars);
+            System.out.println();
+        }
     }
 }
