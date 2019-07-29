ba6991c49f210544b0fe4baafe83befa38dfafeb
giyun <parkgy94@gmail.com>
2019-07-29 22:50:57 +0900

    Feat added the function move and checkMoveRule

--- IdeaProjects/racingCar/src/main/java/model/Car.java
+++ IdeaProjects/racingCar/src/main/java/model/Car.java
@@ -1,21 +1,37 @@
 package model;
 
+import java.util.Random;
+
 public class Car {
+
+    final static int MINIMUM_NUM_OF_MOVE = 4;
 
     private String carName;
 
-    private String position;
+    private int position;
 
     public Car(String carName){
         this.carName=carName;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public String getCarName(){
+        return carName;
     }
 
     public static Car withCarName(String carName){
         return new Car(carName);
     }
 
-    public void move(){
-
+    public void move(int gamePoint){
+        if(checkMoveRule(gamePoint))
+            position++;
+    }
+    public boolean checkMoveRule(int gamePoint){
+        return  MINIMUM_NUM_OF_MOVE<gamePoint;
     }
 
 }
